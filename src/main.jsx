5590c07cbeecafa3f99c8b996a0b561453f965e7
Vitor Queiroz <vitor.qn2004@gmail.com>
2026-06-10 08:58:42 -0300

    Style:packge Lenis

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
 import App from './App.jsx'
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
+
+const lenis = new Lenis({
+  autoRaf: true,
+  autoToggle: true,
+  anchors: true,
+  allowNestedScroll: true,
+  naiveDimensions: true,
+  stopInertiaOnNavigate: true
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
